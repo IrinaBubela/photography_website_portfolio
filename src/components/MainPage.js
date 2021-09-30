a5d889db415b72c1ce7d9644b40abf1bb5fcb145
Iryna Bubela <i.bubela@kjosk.pl>
2021-09-30 16:18:54 +0200

    Viewports style changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,12 +5,12 @@ class MainPage extends React.Component  {
 
   render() {
     return (
-      <div className="homepage">
-        <div className="main-title">
-          <h1><Link to="/photography_website_portfolio/Portfolio">PORTFOLIO</Link></h1>
-          <span><Link to="/photography_website_portfolio/Portfolio">смотреть галерею</Link></span>
+        <div className="main-content">
+          <div className="main-title">
+            <h1><Link to="/photography_website_portfolio/Portfolio">PORTFOLIO</Link></h1>
+            <span><Link to="/photography_website_portfolio/Portfolio">смотреть галерею</Link></span>
+          </div>
         </div>
-      </div>
     );
   }
 }
