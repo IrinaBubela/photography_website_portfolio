f15c58653b7bfea29f6c3766c4fa9a8f60d2cf47
Iryna Bubela <i.bubela@kjosk.pl>
2021-11-07 18:22:07 +0100

    Resized photos and content changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,71 +5,103 @@ function Portfolio()  {
      <div className="portfolio">
          <section className="container section section-image-three">
           <div className="row justify-content-between">
-            <div className="col-12 col-md-4 pb-4 pb-md-1">
-              <img alt="portfolio-1" src={process.env.PUBLIC_URL + '/img/portfolio/ph-1.svg'} className="img img-view lazyloaded" />
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-1" src={process.env.PUBLIC_URL + '/img/portfolio/ph-1.png'} className="img img-view lazyloaded" />
             </div>
-            <div className="col-12 col-md-4 pb-4 pb-md-1">
-              <img alt="portfolio-2" src={process.env.PUBLIC_URL + '/img/portfolio/ph-2.svg'} className="img img-view lazyloaded" />
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-2" src={process.env.PUBLIC_URL + '/img/portfolio/ph-2.png'} className="img img-view lazyloaded" />
             </div>
-            <div className="col-12 col-md-4 pb-4 pb-md-1">
-              <img alt="portfolio-3" src={process.env.PUBLIC_URL + '/img/portfolio/ph-3.svg'} className="img img-view lazyloaded" />
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-3" src={process.env.PUBLIC_URL + '/img/portfolio/ph-3.png'} className="img img-view lazyloaded" />
             </div>
           </div>
         </section>
         <section className="container section section-image-two-type-1">
           <div className="row justify-content-between">
             <div className="col-12 col-md-7 pb-4">
-              <img alt="portfolio-4" src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-2.svg'} className="img img-view lazyloaded" />
+              <img alt="portfolio-4" src={process.env.PUBLIC_URL + '/img/portfolio/ph-4.png'} className="img img-view lazyloaded" />
             </div>
             <div className="col-12 col-md-5 pb-4">
-              <img alt="portfolio-5" src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-3.svg'} className="img img-view lazyloaded" />
+              <img alt="portfolio-5" src={process.env.PUBLIC_URL + '/img/portfolio/ph-5.png'} className="img img-view lazyloaded" />
             </div>
           </div>
         </section>
         <section className="container section section-image-two-type-1">
           <div className="row justify-content-between">
             <div className="col-12 col-md-5 pb-4">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-4.svg'} className="img img-view lazyloaded" />
+              <img alt="portfolio-6" src={process.env.PUBLIC_URL + '/img/portfolio/ph-6.png'} className="img img-view lazyloaded" />
             </div>
             <div className="col-12 col-md-7 pb-4">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-5.svg'} className="img img-view lazyloaded" />
+              <img alt="portfolio-7" src={process.env.PUBLIC_URL + '/img/portfolio/ph-7.png'} className="img img-view lazyloaded" />
             </div>
           </div>
         </section>
         <section className="container section section-image-three">
           <div className="row justify-content-between">
-            <div className="col-12 col-md-4 pb-4 pb-md-1">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-6.svg'} className="img img-view lazyloaded" />
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-8" src={process.env.PUBLIC_URL + '/img/portfolio/ph-8.png'} className="img img-view lazyloaded" />
             </div>
-            <div className="col-12 col-md-4 pb-4 pb-md-1">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-7.svg'} className="img img-view lazyloaded" />
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-9" src={process.env.PUBLIC_URL + '/img/portfolio/ph-9.png'} className="img img-view lazyloaded" />
             </div>
-            <div className="col-12 col-md-4 pb-4 pb-md-1">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-8.svg'} className="img img-view lazyloaded" />
-            </div>
-          </div>
-        </section>
-        <section className="container section section-image-one-v">
-          <div className="row justify-content-center align-items-center">
-            <div className="col-12 col-md-9 pb-4 pd-md-1">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-9.svg'} className="img img-view lazyloaded" />
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-10" src={process.env.PUBLIC_URL + '/img/portfolio/ph-10.png'} className="img img-view lazyloaded" />
             </div>
           </div>
         </section>
         <section className="container section section-image-two-type-1">
           <div className="row justify-content-between">
             <div className="col-12 col-md-7 pb-4">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-10.svg'} className="img img-view lazyloaded" />
+              <img alt="portfolio-11" src={process.env.PUBLIC_URL + '/img/portfolio/ph-11.png'} className="img img-view lazyloaded" />
             </div>
             <div className="col-12 col-md-5 pb-4">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-11.svg'} className="img img-view lazyloaded" />
+              <img alt="portfolio-12" src={process.env.PUBLIC_URL + '/img/portfolio/ph-12.png'} className="img img-view lazyloaded" />
             </div>
           </div>
         </section>
-        <section className="container section section-image-one-v">
-          <div className="row justify-content-center align-items-center">
-            <div className="col-12 col-md-9 pb-4 pd-md-1">
-              <img src={process.env.PUBLIC_URL + '/img/portfolio/portfolio-12.png'} className="img img-view lazyloaded" />
+        <section className="container section section-image-three">
+          <div className="row justify-content-between">
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-13" src={process.env.PUBLIC_URL + '/img/portfolio/ph-13.png'} className="img img-view lazyloaded" />
+            </div>
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-14" src={process.env.PUBLIC_URL + '/img/portfolio/ph-14.png'} className="img img-view lazyloaded" />
+            </div>
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-15" src={process.env.PUBLIC_URL + '/img/portfolio/ph-15.png'} className="img img-view lazyloaded" />
+            </div>
+          </div>
+        </section>
+        <section className="container section section-image-two-type-1">
+          <div className="row justify-content-between">
+            <div className="col-12 col-md-5 pb-4">
+              <img alt="portfolio-16" src={process.env.PUBLIC_URL + '/img/portfolio/ph-16.png'} className="img img-view lazyloaded" />
+            </div>
+            <div className="col-12 col-md-7 pb-4">
+              <img alt="portfolio-17" src={process.env.PUBLIC_URL + '/img/portfolio/ph-17.png'} className="img img-view lazyloaded" />
+            </div>
+          </div>
+        </section>
+        <section className="container section section-image-three">
+          <div className="row justify-content-between">
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-18" src={process.env.PUBLIC_URL + '/img/portfolio/ph-18.png'} className="img img-view lazyloaded" />
+            </div>
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-19" src={process.env.PUBLIC_URL + '/img/portfolio/ph-19.png'} className="img img-view lazyloaded" />
+            </div>
+            <div className="col-12 col-md-4 pb-4">
+              <img alt="portfolio-20" src={process.env.PUBLIC_URL + '/img/portfolio/ph-20.png'} className="img img-view lazyloaded" />
+            </div>
+          </div>
+        </section>
+        <section className="container section section-image-two-type-1">
+          <div className="row justify-content-between">
+            <div className="col-12 col-md-7 pb-4">
+              <img alt="portfolio-21" src={process.env.PUBLIC_URL + '/img/portfolio/ph-21.png'} className="img img-view lazyloaded" />
+            </div>
+            <div className="col-12 col-md-5 pb-4">
+              <img alt="portfolio-22" src={process.env.PUBLIC_URL + '/img/portfolio/ph-22.png'} className="img img-view lazyloaded" />
             </div>
           </div>
         </section>
